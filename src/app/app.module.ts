86bae3260e2345bf987411f35cbd43e0ea65f71e
amohan13 <anandmohanca31@gmail.com>
2018-05-08 15:09:34 +0530

    wheel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -74,7 +74,8 @@ const appRoutes:Routes=[
   //{path:'',redirectTo:'/sidebar' ,pathMatch:'full'},
   //{path:'**',redirectTo:'/sidebar' ,pathMatch:'full'}
   { path:'search/:id1/:id2',component: ShopPageComponent},
-  { path:'search',component: ShopPageComponent}
+  { path:'search',component: ShopPageComponent},
+  { path:'wheel', component: WheelComponent}
 ]
 
 @NgModule({
